6b025f916b261e258908b752e72de2a7ee9b1a1e
kardenvan7 <glushkovlad98@gmail.com>
2025-12-06 04:16:31 +0100

    First small test

--- test/flutter_disco_test.dart
+++ test/flutter_disco_test.dart
@@ -1,1 +1,28 @@
-void main() {}
+import 'package:disco_core/disco_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_disco/flutter_disco.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Entity is registered and retrieved from context', (
+    tester,
+  ) async {
+    final text = '1234';
+    final module = DiscoModule.adHoc((registrar, retriever) {
+      registrar.addSingleton(text);
+    });
+
+    final widget = MaterialApp(
+      builder: (_, _) => DiscoScopeBuilder(
+        create: () => module,
+        builder: (context) {
+          return Text(context.get<String>());
+        },
+      ),
+    );
+
+    await tester.pumpWidget(widget);
+
+    expect(find.text(text), findsOneWidget);
+  });
+}
