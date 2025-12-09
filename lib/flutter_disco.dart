e18dfdff4ea394bccd7d8dfc8737df040ab73d49
kardenvan7 <glushkovlad98@gmail.com>
2025-12-10 00:36:03 +0100

    Exported `disco_core` classes, with the exception of `DiscoScope` and it's descendants, because they're abstracted away by `DiscoScopeBuilder` and context extension

--- lib/flutter_disco.dart
+++ lib/flutter_disco.dart
@@ -1,2 +1,5 @@
+export 'package:disco_core/disco_core.dart'
+    hide DiscoScope, DiscoScopeSync, DiscoScopeAsync;
+
 export 'src/builder.dart' show DiscoScopeBuilder;
 export 'src/context_extension.dart' show FlutterDiscoBuildContextExtension;
